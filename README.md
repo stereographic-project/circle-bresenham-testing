ae3702241366a6aacb7c859eb645bacd4fa196e7
Ugo Brocard <137636358+ugo-brocard@users.noreply.github.com>
2024-01-30 11:55:40 +0100

    :sparkles: update: changing colors for lisibility

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # circle-bresenham-testing
  
 ![Screen](https://github.com/stereographic-project/circle-bresenham-testing/blob/main/images/screen.png)
+![Screen](https://github.com/stereographic-project/circle-bresenham-testing/blob/main/images/screen2.png)
--- src/rendering/circle.py
+++ src/rendering/circle.py
@@ -9,21 +9,25 @@ class Circle:
     center: tuple
     radius: int
 
+    def render_pixels(self, surface: Surface, x, y, color) -> None:
+        surface.set_at((self.center[0] + x, self.center[1] + y), Color(255, 0, 0))
+        surface.set_at((self.center[0] + y, self.center[1] + x), Color(255, 127, 0))
+        surface.set_at((self.center[0] - x, self.center[1] + y), Color(255, 255, 0))
+        surface.set_at((self.center[0] - y, self.center[1] + x), Color(255, 255, 127))
+
+        surface.set_at((self.center[0] + x, self.center[1] - y), Color(255, 255, 255))
+        surface.set_at((self.center[0] + y, self.center[1] - x), Color(127, 255, 255))
+        surface.set_at((self.center[0] - x, self.center[1] - y), Color(0, 255, 255))
+        surface.set_at((self.center[0] - y, self.center[1] - x), Color(0, 127, 255))
+
+
     def render(self, surface: Surface, thickness: int, color: Color) -> None:
         x = 0
         y = self.radius
         m = 5 - 4 * self.radius
         
         while y >= x:
-            surface.set_at((self.center[0] + x, self.center[1] + y), color)
-            surface.set_at((self.center[0] + y, self.center[1] + x), color)
-            surface.set_at((self.center[0] - x, self.center[1] + y), color)
-            surface.set_at((self.center[0] - y, self.center[1] + x), color)
-
-            surface.set_at((self.center[0] + x, self.center[1] - y), color)
-            surface.set_at((self.center[0] + y, self.center[1] - x), color)
-            surface.set_at((self.center[0] - x, self.center[1] - y), color)
-            surface.set_at((self.center[0] - y, self.center[1] - x), color)
+            self.render_pixels(surface, x, y, color)
 
             if m > 0:
                 y -= 1
